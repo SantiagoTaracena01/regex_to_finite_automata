2fc70b5633644d54d71e539c358f4f25227848cb
unknown <staracenapuga@gmail.com>
2022-08-23 12:24:42 -0600

    Repositorio inicializado.

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from regex import Regex
+
+if __name__ == "__main__":
+  my_regex: Regex = Regex("(a|b)*abb")
---
+++ src/regex.py
@@ -0,0 +1,3 @@
+class Regex(object):
+  def __init__(self, expression):
+    self.__expression = expression
