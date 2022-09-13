5d6ae7e81fd12e48d3219aef6c89f92098d839ed
unknown <staracenapuga@gmail.com>
2022-09-12 19:03:46 -0600

    Avances en el algoritmo de Thompson.

--- src/finite_automata.py
+++ src/finite_automata.py
@@ -1,6 +1,6 @@
 from set import PersonalSet
 
-EPSILON: str = "E"
+EPSILON = "E"
 
 class NFA(object):
 
@@ -10,73 +10,77 @@ class NFA(object):
   __initial_state = None
   __acceptance_states = PersonalSet([])
 
+  # Método constructor del AFN.
   def __init__(self, regex):
-    created_states = 0
+    
+    # Creación del estado inicial.
+    created_state = 0
+    self.__initial_state = created_state
+    
+    # Ciclo for que itera la expresión regular a convertir.
     for index, char in enumerate(regex):
-      print(index, char)
+      
+      # Si el caracter de la expresión regular es un *, creamos un barco.
       if (char == "*"):
-        for i in range(4):
-          self.__states.add(created_states)
-          if (i == 0):
-            self.__mapping.add({
-              created_states: {
-                EPSILON: [(created_states + 1), (created_states + 3)]
-              }
-            })
-          elif (i == 1):
-            self.__mapping.add({
-              created_states: {
-                regex[index - 1]: [(created_states + 1)]
-              }
-            })
-          elif (i == 2):
-            self.__mapping.add({
-              created_states: {
-                EPSILON: [(created_states - 2), (created_states + 1)]
-              }
-            })
-          elif (i == 3):
-            self.__mapping.add({
-              created_states: {
-                0: None
-              }
-            })
-          created_states += 1
+        created_state = self.__ship(regex, index, created_state)
+      
+      # Para cualquier caracter del alfabeto, hacemos el proceso siguiente.
       else:
-        if (char not in self.__alphabet):
+        
+        # Si el caracter aún no está en el alfabeto, este se agrega al mismo.
+        if (char not in self.__alphabet.get_content()):
           self.__alphabet.add(char)
-    self.__initial_state = self.__states[0]
-    self.__acceptance_states = self.__states[-1]
+        
+        
+        if (regex[index - 1] == "*"):
+          self.__states.add(created_state)
+        try:
+          next_char = regex[index + 1]
+          if ((next_char != "*") and (next_char != "+") and (next_char != "(") and (next_char != ")")):
+            self.__mapping[created_state] = { regex[index + 1]: [(created_state + 1)] }
+          else:
+            self.__mapping[created_state] = { EPSILON: [(created_state + 1)] }
+        except:
+          self.__mapping[created_state] = {}
+        created_state += 1
+    
+    # Búsqueda y creación del estado de aceptación del AFN.
+    for key in self.__mapping:
+      if (self.__mapping[key] == {}):
+        self.__acceptance_states.add(key)
+
+  def __ship(self, regex, index, state):
+    for i in range(4):
+      self.__states.add(state)
+      if (i == 0):
+        self.__mapping[state] = { EPSILON: [(state + 1), (state + 3)], }
+      elif (i == 1):
+        self.__mapping[state] = { regex[index - 1]: [(state + 1)] }
+      elif (i == 2):
+        self.__mapping[state] = { EPSILON: [(state - 1), (state + 1)] }
+      else:
+        try:
+          next_char = regex[index + 1]
+          try:
+            second_next_char = regex[index + 2]
+            if (second_next_char == "*"):
+              self.__mapping[state] = { EPSILON: [(state + 1)] }
+              break
+          except:
+            self.__mapping[state] = { next_char: [(state + 1)] }
+        except: 
+          self.__mapping[state] = {}
+      state += 1
+    return state
 
   def __str__(self):
-    return f"""
-      States: {self.__states}
-      Alphabet: {self.__alphabet}
-      Mapping: {self.__mapping}
-    """
+    return f"States: {self.__states}\nInitial State: {self.__initial_state}\nAcceptance States: {self.__acceptance_states}\nAlphabet: {self.__alphabet}\nMapping: {self.__mapping}\n"
 
 class DFA(object):
-  
+
   def __init__(self, regex):
     for char in regex:
       print(char)
-
-# from visual_automata.fa.nfa import VisualNFA
-
-# # a*
-
-# nfa = VisualNFA(
-#   states={"0", "1", "2", "3"},
-#   input_symbols={"a"},
-#   transitions={
-#     "0": { "": {"1", "3"} },
-#     "1": { "a": {"2"} },
-#     "2": { "": {"1", "3"} },
-#     "3": {}
-#   },
-#   initial_state="0",
-#   final_states={"3"},
-# )
 
 """
 nfa = NFA(
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from finite_automata import NFA
 
 if (__name__ == "__main__"):
   
-  regex: str = input("Ingresa una expresión regular: ")
+  regex: str = input("\nIngresa una expresión regular: ")
   regex_chain: str = input("Ingresa una cadena: ")
   my_nfa: NFA = NFA(regex)
   print(my_nfa)
